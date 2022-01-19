4cf39bfafacb0bc23764b14d48b864a48fe52368
Emiller8 <Ericm2195@outlook.com>
2022-01-18 21:06:55 -0600

    This is the first commit

---
+++ apps/apptemplate/assignment01.cpp
@@ -0,0 +1,16 @@
+//
+// File:   assignment1.cpp
+// Author: Eric Miller
+// Purpose:
+// Illustrate some of the bad thins that can happen with
+// pointers
+//
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  // Put your code here.
+	cout << "Hello World";
+  return 0;
+}
