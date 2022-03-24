584666c150f725acf79048a304bedcfc4d86d6d7
Eric Miller <58384595+Attkless@users.noreply.github.com>
2022-03-24 04:19:19 -0500

    push

--- apps/Assignment07/Assignment07/Assignment07.cpp
+++ apps/Assignment07/Assignment07/Assignment07.cpp
@@ -26,63 +26,211 @@
 */
 
 #include <iostream>
-#include <algorithm>
-#include <chrono> 
-#include <cmath>
-#include <vector>
-#include <iomanip>
+#include <cstdlib>
+#include <chrono>
 
-using namespace std::chrono;
 using namespace std;
+using namespace std::chrono;
 
-int anyEqual(int n, int A[][1000]) {
-	int i, j, k, m;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			for (int k = 1; k <= n; k++)
-				for (int m = 1; m <= n; m++)
-					if (A[i][j] == A[k][m] && !(i == k && j ==m ))
-						return 1;
-	return 0;
+// int n = the size of the array being passed in 
+/*int anyEqualTen(int n, int A[][10]) {
+    int i, j, k, m;
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            for (k = 1; k <= n; k++)
+                for (m = 1; m <= n; m++)
+                     if (A[i][j] == A[k][m] && !(i == k && j == m))
+                        return 1;
+    return 0;
+}
+*/
+void generateArrayTen(int array[][10], int size) {
+    double average = 0; 
+    double sum = 0; 
+    auto start = steady_clock::now(); 
+    int count = 0; 
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+                cout << array[i][j] << endl;
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
 }
 
-void generateArray(int a[], int size) {
-	double average = 0;
-	double sum = 0;
-	auto start = steady_clock::now();
-	int random = 20;
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < random; j++)
-			a[i] = (rand() % random);
-	auto end = steady_clock::now();
-	duration<double> elapsed_seconds = end - start;
-	sum = sum + elapsed_seconds.count();
-	cout << "Time taken by function: " << size << " " << (sum / random) << endl;
+void generateArrayTwenty(int array[][20], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+
 }
 
-int main() { 
-	const int SIZE1 = 10; 
-	const int SIZE2 = 20;
-	const int SIZE3 = 30;
-	const int SIZE4 = 40; 
-	const int SIZE5 = 50;
-	const int SIZE6 = 100;
-	const int SIZE7 = 500;
-	const int SIZE8 = 1000;
-	int myList1[SIZE1];
-	int myList2[SIZE2];
-	int myList3[SIZE3];
-	int myList4[SIZE4];
-	int myList5[SIZE5];
-	int myList6[SIZE6];
-	int myList7[SIZE7];
-	int myList8[SIZE8];
-	generateArray(myList1, SIZE1);
-	generateArray(myList2, SIZE2);
-	generateArray(myList3, SIZE3);
-	generateArray(myList4, SIZE4);
-	generateArray(myList5, SIZE5);
-	generateArray(myList6, SIZE6);
-	generateArray(myList7, SIZE7);
-	generateArray(myList8, SIZE8);
+void generateArrayThirty(int array[][30], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+void generateArrayFourty(int array[][40], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+void generateArrayFifty(int array[][50], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+void generateArrayOneHundred(int array[][100], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+void generateArrayFiveHundred(int array[][500], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+void generateArrayOneThousand(int array[][1000], int size) {
+    double average = 0;
+    double sum = 0;
+    auto start = steady_clock::now();
+    int count = 0;
+    int random = 20;
+    while (count < 20) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (rand() % random);
+            }
+        }
+        auto end = steady_clock::now();
+        duration<double> elapsed_seconds = end - start;
+        sum = sum + elapsed_seconds.count();
+        count++;
+    }
+    cout << "Time taken by function: " << size << " " << (sum / size) << endl;
+}
+
+
+int main()
+{
+    const int SIZE1 = 10;
+    const int SIZE2 = 20;
+    const int SIZE3 = 30;
+    const int SIZE4 = 40; 
+    const int SIZE5 = 50;
+    const int SIZE6 = 100;
+    const int SIZE7 = 500;
+    const int SIZE8 = 1000;
+    int array[10][10];
+    int array2[SIZE2][SIZE2];
+    int array3[SIZE3][SIZE3];
+    int array4[SIZE4][SIZE4];
+    int array5[SIZE5][SIZE5];
+    int array6[SIZE6][SIZE6];
+    int array7[SIZE7][SIZE7];
+    int array8[SIZE8][SIZE8]; 
+    generateArrayTen(array, 10);
+    generateArrayTwenty(array2, SIZE2);
+    generateArrayThirty(array3, SIZE3);
+    generateArrayFourty(array4, SIZE4);
+    generateArrayFifty(array5, SIZE5);
+    generateArrayOneHundred(array6, SIZE6);
+    generateArrayFiveHundred(array7, SIZE7);
+    generateArrayOneThousand(array8, SIZE8);
 }
