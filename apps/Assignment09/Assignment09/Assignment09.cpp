c9bcef16e7d982e37a91d32677412478e50b0866
Eric Miller <58384595+Attkless@users.noreply.github.com>
2022-04-12 19:26:32 -0500

    push to pull onto other device

--- apps/Assignment09/Assignment09/Assignment09.cpp
+++ apps/Assignment09/Assignment09/Assignment09.cpp
@@ -1,7 +1,16 @@
 ﻿// Assignment09.cpp : Defines the entry point for the application.
-//
+/*  (30 points) Write two functions that accepts a reference to a binary search tree as 
+	input and returns a reference to a list with the items sorted in (a) ascending order,
+	and (b) descending order. (HINT: traversal of the tree)
+	
+	(30 points) In Assignment 04, your wrote a program to keep track of the tasks you 
+	needed to complete to finish an assignment. Use the C++ STL heap and priority queue
+	classes to keep the list sorted in priority data. You may modify the data file to 
+	include an integer priority as the last value. Modify the menu to allow the user to 
+	adjust the priority of task. 
+*/
 
-#include "Assignment09.h"
+#include <iostream>
 
 using namespace std;
 
